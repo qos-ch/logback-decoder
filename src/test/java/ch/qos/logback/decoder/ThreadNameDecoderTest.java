a70d8c4a0eaa01750d9dd142198d3e5764aa6dda
Tony Trinh <tony19@gmail.com>
2013-07-21 20:23:53 -0500

    Add test cases to ThreadNameDecoderTest

--- src/test/java/ch/qos/logback/decoder/ThreadNameDecoderTest.java
+++ src/test/java/ch/qos/logback/decoder/ThreadNameDecoderTest.java
@@ -25,14 +25,32 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
  * @author Anthony Trinh
  */
 public class ThreadNameDecoderTest extends DecoderTest {
+  @Test
+  public void decodesNumericThreadName() {
+    assertEquals("123", getThreadName("123"));
+  }
 
   @Test
-  public void decodesThreadName() {
-    final String INPUT = "2013-06-12 15:27:15.044 INFO  [main] KdbFxFeedhandlerApp: Running com.ubs.sprint.kdb.fx.feedhandler.server.KdbFxFeedhandlerApp from directory: /sbclocal/sprint/kdb-fx-feedhandler/0.0.23/bin/.\n";
-    final String PATT = "%d{yyyy-MM-dd HH:mm:ss.SSS} %-5level [%thread] %logger{0}: %msg%n";
+  public void decodesQualifiedClassNameAsThreadName() {
+    assertEquals("com.example.foo", getThreadName("com.example.foo"));
+  }
+
+  @Test
+  public void decodesLabelWithIndexAsThreadName() {
+    assertEquals("Thread-0", getThreadName("Thread-0"));
+  }
+
+  @Test
+  public void decodesSentenceAsThreadName() {
+    assertEquals("thread name can be any non-empty string", getThreadName("thread name can be any non-empty string"));
+  }
+
+  private String getThreadName(String name) {
+    final String INPUT = "2013-06-12 15:27:15.044 INFO  <" + name + ">: foo bar message\n";
+    final String PATT = "%d{yyyy-MM-dd HH:mm:ss.SSS} %-5level <%thread{0}>: %msg%n";
     decoder.setLayoutPattern(PATT);
     ILoggingEvent event = decoder.decode(INPUT);
     assertNotNull(event);
-    assertEquals("main", event.getThreadName());
+    return event.getThreadName();
   }
 }

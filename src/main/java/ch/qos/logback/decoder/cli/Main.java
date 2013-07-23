4c2a214df01376c9135febddf3f7280242b8650a
Tony Trinh <tony19@gmail.com>
2013-07-22 19:44:35 -0500

    Move Main class to cli package

--- src/main/java/ch/qos/logback/decoder/cli/Main.java
+++ src/main/java/ch/qos/logback/decoder/cli/Main.java
@@ -10,23 +10,25 @@
  * under the terms of the GNU Lesser General Public License version 2.1
  * as published by the Free Software Foundation.
  */
-package ch.qos.logback.decoder;
+package ch.qos.logback.decoder.cli;
 
 import java.io.File;
+
+import ch.qos.logback.decoder.FileDecoder;
 
 /**
  * Provides application entry point
  */
 public final class Main {
-  
+
   /**
    * Constructor not called
    */
   private Main() {}
-  
+
   /**
    * Entry point for command-line interface
-   * 
+   *
    * @param args the command-line parameters
    */
   static public void main(String[] args) {
